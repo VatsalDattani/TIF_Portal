b45740709bd23afd2e24465189a22f0ca820cafc
Vatsal Dattani <29160800+VatsalDattani@users.noreply.github.com>
2019-10-05 21:35:24 +0530

    Logos inserted and footer component intialized

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+export default () => {
+    <div>
+        <ul>
+            <li><img src="../static/logo.png" /></li>
+            <li><img src /></li>
+            <li><img src /></li>
+            <li><img src /></li>
+        </ul>
+    </div>
+}
